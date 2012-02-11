e034479f6b2d5864a47142a0b7eb08d5b89e95fa
Junichiro Takagi <t.junichiro@gmail.com>
2012-02-12 01:05:30 +0900

    modify emit format

--- lib/fluent/plugin/out_kestrel.rb
+++ lib/fluent/plugin/out_kestrel.rb
@@ -1,17 +1,21 @@
 module Fluent
   class KestrelOutput < BufferedOutput
     Fluent::Plugin.register_output('kestrel', self)
+    attr_reader :kestrel
 
     config_param :host,         :string,    :default => nil
     config_param :port,         :integer,   :default => 22133
     config_param :queue,        :string,    :default => nil
     config_param :ttl,          :integer,   :default => 0
     config_param :raw,          :bool,      :default => false
+    config_param :time_format,  :string,    :default => nil
+
 
 
     def initialize
       super
       require 'kestrel'
+      require 'time'
     end
 
     def configure(conf)
@@ -20,12 +24,13 @@ module Fluent
       unless @queue && @host
         raise ConfigError, "'host' and 'queue' parameter must be specified."
       end
+      @timef = TimeFormatter.new(@time_format, @localtime)
     end
 
     def start
       super
 
-      @kestrel = Kestrel::Client.new(@host + ":" + @port)
+      @kestrel = Kestrel::Client.new(@host + ":" + @port.to_s)
     end
 
     def shutdown
@@ -33,12 +38,21 @@ module Fluent
     end
 
     def format(tag, time, record)
-      "#{time}\t#{tag}\t#{record.to_json}"
+      [tag, time, record].to_msgpack
     end
 
     def write(chunk)
-      chunk.open{ |data|
-          @kestrel.set(@queue, data, ttl=@ttl, raw=@raw)
+      chunk.open { |io|
+        begin
+          MessagePack::Unpacker.new(io).each{ |tag, time, record|
+            time_str = @timef.format(time)          
+            data = "#{time_str}\t#{tag}\t#{record.to_json}"
+
+            @kestrel.set(@queue, data, ttl=@ttl, raw=@raw)
+          }
+        rescue EOFError
+          # EOFError always occured when reached end of chunk.          
+        end
       }
     end
   end
