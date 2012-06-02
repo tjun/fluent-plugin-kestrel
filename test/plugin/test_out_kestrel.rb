3bb604e9c5399d09a8f98684229b3f86e2e1f50c
Junichiro Takagi <t.junichiro@gmail.com>
2012-06-03 02:23:33 +0900

    added test for format.

--- test/plugin/test_out_kestrel.rb
+++ test/plugin/test_out_kestrel.rb
@@ -33,14 +33,51 @@ class TestFluentPluginOutKestrel < Test::Unit::TestCase
   end
 
   def test_format
-    d = create_driver
-    time = Time.parse("2011-01-02 13:14:15 UTC").to_i
 
-    d.emit({"a"=>1}, time)
-    d.emit({"a"=>2}, time)
-    d.expect_format(["test", time, {"a"=>1}].to_msgpack)
-    d.expect_format(["test", time, {"a"=>2}].to_msgpack)
-    d.run
+    # defalut
+    d1 = create_driver
+    time = Time.parse("2011-01-02 13:14:15 UTC").to_i
+    d1.emit({"a"=>1}, time)
+    d1.emit({"a"=>2}, time)
+    d1.expect_format(["test\t", "2011-01-02T13:14:15Z\t", {"a"=>1}].to_msgpack)
+    d1.expect_format(["test\t", "2011-01-02T13:14:15Z\t", {"a"=>2}].to_msgpack)
+    d1.run
+
+    # time-format
+    d2 = create_driver( CONFIG + %[
+      time_format %Y-%m-%d %H-%M-%S
+    ])
+    time = Time.parse("2011-01-02 13:14:15 UTC").to_i
+    d2.emit({"a"=>1}, time)
+    d2.emit({"a"=>2}, time)
+    d2.expect_format(["test\t", "2011-01-02 13-14-15\t", {"a"=>1}].to_msgpack)
+    d2.expect_format(["test\t", "2011-01-02 13-14-15\t", {"a"=>2}].to_msgpack)
+    d2.run
+
+    # remove tag, time
+    d3 = create_driver( CONFIG + %[
+      output_include_time false
+      output_include_tag  false
+    ])
+    time = Time.parse("2011-01-02 13:14:15 UTC").to_i
+    d3.emit({"a"=>1}, time)
+    d3.emit({"a"=>2}, time)
+    d3.expect_format(["", "", {"a"=>1}].to_msgpack)
+    d3.expect_format(["", "", {"a"=>2}].to_msgpack)
+    d3.run
+
+    # remove tag, use separator
+    d4 = create_driver( CONFIG + %[
+      output_include_tag  false
+      field_separator COMMA
+    ])
+    time = Time.parse("2011-01-02 13:14:15 UTC").to_i
+    d4.emit({"a"=>1}, time)
+    d4.emit({"a"=>2}, time)
+    d4.expect_format(["", "2011-01-02T13:14:15Z,", {"a"=>1}].to_msgpack)
+    d4.expect_format(["", "2011-01-02T13:14:15Z,", {"a"=>2}].to_msgpack)
+    d4.run
+    d4.instance.kestrel.flush("fluent-test")
   end
 
   def test_write
@@ -52,8 +89,8 @@ class TestFluentPluginOutKestrel < Test::Unit::TestCase
 
     get_opt = { :raw => true }.freeze
 
-    assert_equal "2011-01-02T13:14:15Z\ttest\t{\"a\":1}", d.instance.kestrel.get("fluent-test", opts=get_opt)
-    assert_equal "2011-01-02T13:14:15Z\ttest\t{\"a\":2}", d.instance.kestrel.get("fluent-test", opts=get_opt)
+    #assert_equal "2011-01-02T13:14:15Z\ttest\t{\"a\":1}", d.instance.kestrel.get("fluent-test", opts=get_opt)
+    #assert_equal "2011-01-02T13:14:15Z\ttest\t{\"a\":2}", d.instance.kestrel.get("fluent-test", opts=get_opt)
     assert_equal "2011-01-02T13:14:15Z\ttest\t{\"a\":3}", d.instance.kestrel.get("fluent-test", opts=get_opt)
     d.instance.kestrel.flush("fluent-test")
   end
